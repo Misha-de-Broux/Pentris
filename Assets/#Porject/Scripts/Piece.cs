03be7c419f16c0d6d5ae4e6cb2c65d6364417baf
DE BROUX Misha <stg2895@brufor.lan>
2024-07-01 14:17:13 +0200

    v1

--- Assets/#Porject/Scripts/Piece.cs
+++ Assets/#Porject/Scripts/Piece.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    [SerializeField] float speed = 1;
+    public float currentSpeed = 0;
     public void Fall() {
-        Destroy(gameObject);
+        currentSpeed = speed;
+    }
+
+    public void Update() {
+        transform.Translate(Vector3.down * currentSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject.name);
+        if(other.CompareTag(tag)) {
+            if (other.transform.parent != transform) {
+                List<Transform> cubes = new List<Transform>(GetComponentsInChildren<Transform>());
+                foreach (Transform cube in cubes) {
+                    cube.parent = transform.parent;
+                    Vector3 position = cube.position;
+                    position.y -= position.y % 0.2f + 0.3f;
+                    cube.position = position;
+                }
+                Destroy(gameObject);
+            }
+        }
     }
 }
